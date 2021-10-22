af36f74bc3536af6652e11eaf3184e0c974a2bbe
Meer Shah <mshah229@uic.edu>
2021-10-22 07:34:50 -0500

    Barely functional routes

---
+++ README.md
@@ -0,0 +1,21 @@
+####Creator - Meer Piyush Shah
+
+####Development Tools and Packages:
+* Editor - [Atom](https://atom.io/)
+* Browser - [Firefox](https://www.mozilla.org/en-US/firefox/new/)
+* [Node.js](https://nodejs.org/en/) Packages:
+  * [Express.js](https://expressjs.com/)
+  * [Passport.js](https://www.passportjs.org/)
+  * For an exhaustive list, refer the package.json file.
+
+####Reference and Tutorials:
+* Node and Express - [FreeCodeCamp](https://www.youtube.com/watch?v=Oe421EPjeBE)
+* Passport.js - [FreeCodeCamp](https://www.youtube.com/watch?v=F-sFp_AvHc8)
+* FrontEnd Reference - [MDN](https://developer.mozilla.org/en-US/docs/Learn)
+
+####Credits:
+* FrontEnd Template - [HTML5 Up](https://html5up.net/story)
+* Homepage Video - [BBC Earth](https://www.bbcearth.com/shows/serengeti-ii)
+* Logo - [Tree Favicon](https://www.favicon.cc/?action=icon&file_id=795443)
+* Title Font - [Twiggy](https://www.dafont.com/twiggy.font)
+* Icons - [FlatIcon](https://www.flaticon.com/authors/kiranshastry)
--- app.js
+++ app.js
@@ -5,22 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +36,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(5000, ()=>{
+  console.log("Listening on port 5000.");
 });
 
 module.exports = app;
